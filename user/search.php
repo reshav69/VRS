d556532414b4d33b88dd1f8c427c0ab658996c87
Pujan Maharjan <pujanmaharjan22@gmail.com>
2024-04-28 15:19:56 +0545

    footer, search page

--- user/search.php
+++ user/search.php
@@ -30,22 +30,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Search</title>
+    <link rel="stylesheet" href="../css/search.css">
     <link rel="stylesheet" href="../css/view.css">
 </head>
 <body>
 	<?php include 'user-nav.php'; ?>
     <h1>Search Vehicle</h1>
     <form action="search.php" method="post">
-        <label for="searchInp">Enter the name of the vehicle: </label>
-        <input type="text" name="searchInp" id="sinp" value="<?php echo htmlspecialchars($searchV); ?>">
-        <label for="type">Choose the type of the vehicle: </label>
-        <select name="type">
-            <option value="">--Choose One--</option>
-            <option value="bicycle" <?php echo ($vType == 'bicycle') ? 'selected' : ''; ?>>Bicycle</option>
-            <option value="car" <?php echo ($vType == 'car') ? 'selected' : ''; ?>>Car</option>
-			<option value="bike" <?php echo ($vType == 'bike') ? 'selected' : ''; ?>>Bike</option>            
-        </select>
-        <input type="submit" name="searchbtn" id="sbtn" class="searchBtn" value="Search">
+        <div class="searchbox">
+        <!-- <label for="searchInp">Enter the name of the vehicle: </label> -->
+            <img src="../images/search-icon.webp" id="icon">
+            <input type="text" name="searchInp" id="sinp" value="<?php echo htmlspecialchars($searchV); ?>" placeholder="Enter name of vehicle">
+        </div>
+        <!-- <label for="type">Choose the type of the vehicle: </label> -->
+        <div class="searchbox">
+            <select name="type">
+                <option value="">Category</option>
+                <option value="bicycle" <?php echo ($vType == 'bicycle') ? 'selected' : ''; ?>>Bicycle</option>
+                <option value="car" <?php echo ($vType == 'car') ? 'selected' : ''; ?>>Car</option>
+                <option value="bike" <?php echo ($vType == 'bike') ? 'selected' : ''; ?>>Bike</option>            
+            </select>
+        </div>
+        <button type="submit" name="searchbtn" id="sbtn" class="searchBtn">Search</button>
+        </div>
     </form>
 
     <?php if ($_SERVER["REQUEST_METHOD"] == "POST" && $result) : ?>
@@ -55,7 +62,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <div class="vehicle-container">
                 <div class="box">
                     <div class="img-container">
-                        <img src="../vehicleImages/<?php echo $vehicle['image_filename']?>" alt="vehicleimage">
+                    <a href="user-vehicle.php?id=<?php echo $vehicle['vehicle_id']; ?>"><img src="../vehicleImages/<?php echo $vehicle['image_filename']?>" alt="vehicleimage"></a>
                     </div>
                     <div class="model-info">
                         <li>model:<?php echo $vehicle['model']; ?> </li>
